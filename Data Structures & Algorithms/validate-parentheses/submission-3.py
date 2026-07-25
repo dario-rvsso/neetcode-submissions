f0db82a3b221fbef303f227d7eb44c6b487aafdd
dario-rvsso <58913887+dario-rvsso@users.noreply.github.com>
2026-07-25 17:07:47 +0200

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,37 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        n = len(s)
+        molecule = []
+        valid = True
+        try:
+            for i in range(n):
+                atom = s[i]
+                match atom:
+                    case '(' | '[' | '{':
+                        molecule.append(atom)
+                    case ')':
+                        x = molecule.pop()
+                        if x != '(':
+                            valid = False
+                            break
+                    case ']':
+                        x = molecule.pop()
+                        if x != '[':
+                            valid = False
+                            break
+                    case '}':
+                        x = molecule.pop()
+                        if x != '{':
+                            valid = False
+                            break
+            if len(molecule) != 0:
+                valid = False
+        except IndexError:
+            valid = False
+        #finally:
+        #    pass
+        
+        return valid
+                            
+
+                    
