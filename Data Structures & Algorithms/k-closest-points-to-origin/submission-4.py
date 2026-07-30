bd5e0357c38757d09bc29d562301d5bfb040066b
dario-rvsso <58913887+dario-rvsso@users.noreply.github.com>
2026-07-30 18:11:20 +0200

    Add: k-closest-points-to-origin - submission-4

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-4.py
@@ -0,0 +1,43 @@
+from math import sqrt
+from random import randint
+
+class Solution:
+    def qsort(self, points: List[List[int]], distances: List[float], s: int, e: int, k: int) -> None:
+        if s >= e:
+            return
+
+        m = randint(s, e)
+        pivot = distances[m]
+        i = s
+        j = e
+        while i <= j:
+            while distances[i] < pivot:
+                i += 1
+            while distances[j] > pivot:
+                j -= 1
+            if i <= j:
+                tmp = points[i]
+                points[i] = points[j]
+                points[j] = tmp
+                dtp = distances[i]
+                distances[i] = distances[j]
+                distances[j] = dtp
+                i += 1
+                j -= 1
+        
+        if k <= j:
+            self.qsort(points, distances, s, j, k)
+        elif k >= i:
+            self.qsort(points, distances, i, e, k)
+
+        return
+
+
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        distances = []
+        for i in range(len(points)):
+            distances.append(points[i][0]**2 + points[i][1]**2)
+        
+        self.qsort(points, distances, 0, len(points)-1, k - 1)
+        
+        return points[:k]
