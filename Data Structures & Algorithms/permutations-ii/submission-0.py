11f0b9a5d3da71356b81c70a9a05ce7f2e315840
dario-rvsso <58913887+dario-rvsso@users.noreply.github.com>
2026-09-14 18:02:09 +0200

    Add: permutations-ii - submission-0

---
+++ Data Structures & Algorithms/permutations-ii/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+
+        def helper(i, nums):
+            if i == len(nums):
+                return [[]]
+
+            resPerms = []
+            perms = helper(i + 1, nums)
+            for p in perms:
+                for j in range(len(p) + 1):
+                    pCopy = p.copy()
+                    pCopy.insert(j, nums[i])
+                    if pCopy not in resPerms:
+                        resPerms.append(pCopy)
+            return resPerms
+
+
+        return helper(0, nums)
